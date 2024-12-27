faf128d90dfd44951aa9e4f790d76cbd12a46324
Abhijeet Karlewar <abhijeet.dipak@netcomlearning.com>
2024-12-27 11:30:24 +0530

    Changed the Hero component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,9 @@ const Hero = () => {
 	return (
 		<div className="flex flex-col sm:flex-row border border-gray-400 ">
 			{/* Hero Left Side */}
+			<img src={assets.hero_img2} className="w-full sm:w-1/2" alt="" />
 
+			{/* Hero Right Side */}
 			<div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
 				<div className="text-[#414141]">
 					<div className="flex items-center gap-2">
@@ -21,9 +23,6 @@ const Hero = () => {
 					</div>
 				</div>
 			</div>
-
-			{/* Hero Right Side */}
-			<img src={assets.hero_img2} className="w-full sm:w-1/2" alt="" />
 		</div>
 	);
 };
